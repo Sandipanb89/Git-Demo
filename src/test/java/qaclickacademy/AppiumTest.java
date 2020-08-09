4ade215a39e9de9b5f0f20843415ff414f92635e
sandipan <sandipanb@MPTDC1-India.MirrorPlus.com>
2020-08-09 20:13:55 +0530

    commit by user 2

--- src/test/java/qaclickacademy/AppiumTest.java
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -21,6 +21,11 @@ public class AppiumTest {
 		System.out.println("Git commit 3 by Sandy");
 		System.out.println("Git commit 4 by Sandy");
 		System.out.println("Git commit 5 by Sandy");
+		
+		System.out.println("Git commit 10 by GITdemo");
+		System.out.println("Git commit 11 by GITdemo");
+		System.out.println("Git commit 12 by GITdemo");
+		System.out.println("Git commit 13 by GITdemo");
 	}
 
 }
